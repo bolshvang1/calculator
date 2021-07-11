f6b5ed0d3aac5135e282bebd405cce059edd38da
bolshvang <brandonolshvang@gmail.com>
2021-07-11 13:21:30 -0400

    added absolute path and fixed Calc tests

--- src/Tests/test_calculatorTest.py
+++ src/Tests/test_calculatorTest.py
@@ -1,6 +1,7 @@
 import unittest
 from src.Calculator.calculator import Calculator
 from src.CsvReader import CsvReader
+from src.Fileutilities.absolutepath import absolutepath
 
 
 class MyTestCase(unittest.TestCase):
@@ -11,37 +12,37 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_subtraction(self):
-        test_data = CsvReader.CsvReader('/src/Tests/Data/subtraction.csv').data
+        test_data = CsvReader.CsvReader(absolutepath('/src/Tests/Data/subtraction.csv')).data
         for row in test_data:
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_division(self):
-        test_data = CsvReader.CsvReader('/src/Tests/Data/division.csv').data
+        test_data = CsvReader.CsvReader(absolutepath('/src/Tests/Data/division.csv')).data
         for row in test_data:
             self.assertAlmostEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertAlmostEqual(self.calculator.result, float(row['Result']))
 
     def test_multiplication(self):
-        test_data = CsvReader.CsvReader('/src/Tests/Data/multiplication.csv').data
+        test_data = CsvReader.CsvReader(absolutepath('/src/Tests/Data/multiplication.csv')).data
         for row in test_data:
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_squareroot(self):
-        test_data = CsvReader.CsvReader('/src/Tests/Data/squareroot.csv').data
+        test_data = CsvReader.CsvReader(absolutepath('/src/Tests/Data/squareroot.csv')).data
         for row in test_data:
             self.assertAlmostEqual(self.calculator.squareroot(row['Value 1']), float(row['Result']))
             self.assertAlmostEqual(self.calculator.result, float(row['Result']))
 
     def test_square(self):
-        test_data = CsvReader.CsvReader('/src/Tests/Data/square.csv').data
+        test_data = CsvReader.CsvReader(absolutepath('/src/Tests/Data/square.csv')).data
         for row in test_data:
             self.assertEqual(self.calculator.square(row['Value 1']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_addition(self):
-        test_data = CsvReader.CsvReader('/src/Tests/Data/addition.csv').data
+        test_data = CsvReader.CsvReader(absolutepath('/src/Tests/Data/addition.csv')).data
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
